4ee6edad1a1c434176a6105fb8c655a17500f649
Brais Cidras <braisomary@gmail.com>
2022-03-24 09:01:29 +0100

    Implementing a side effects, final aproach (using ngrx effects)

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Actions, ofType } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { pipe } from "rxjs";
 import { tap } from "rxjs/operators";
 import { AuthActions } from "./action-types";
@@ -7,15 +7,14 @@ import { AuthActions } from "./action-types";
 @Injectable()
 export class AuthEffects {
 
-    constructor(private actions$: Actions) {
+    login$ = createEffect(() => this.actions$
+    .pipe(
+        ofType(AuthActions.login),
+        tap(action => localStorage.setItem('user', JSON.stringify(action.user)))
+    ),
+    {dispatch: false}); // This is required, because normally a effect triggers a new action but here is not the case
 
-        const login$ = this.actions$
-            .pipe(
-                ofType(AuthActions.login),
-                tap(action => localStorage.setItem('user', JSON.stringify(action.user)))
-            );
-
-        login$.subscribe();
+    constructor(private actions$: Actions) {     
     }
 
 }
